fb9a2bacc254bd188246e5179c6472428a194954
Gabriel Engel <gabriel@engel.dev.br>
2025-04-25 19:54:05 -0300

    feat: integrate Tailwind CSS and clean files to start frontend

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+
+  const env = loadEnv(mode, mode === 'development'
+    ? process.cwd().replace('/apps/frontend', '')
+    : process.cwd())
+
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+
+    server: {
+      open: true,
+      host: true,
+      port: Number(env.VITE_PORT),
+    },
+    preview: {
+      open: false,
+      port: Number(env.VITE_PORT),
+    },
+    resolve: {
+      alias: {
+
+        '@': '/src',
+        '@lib': '/src/lib',
+        '@components': '/src/view/components',
+      },
+    },
+  }
 })
