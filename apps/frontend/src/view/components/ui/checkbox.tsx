2922369ebb008a5e91e5d68c340457bd15510b54
Gabriel Engel <gabriel@engel.dev.br>
2025-04-25 21:45:53 -0300

    feat: enhance frontend components with new UI elements, integrate Radix UI components, and update dependencies

---
+++ apps/frontend/src/view/components/ui/checkbox.tsx
@@ -0,0 +1,35 @@
+import * as CheckboxPrimitive from "@radix-ui/react-checkbox"
+import { CheckIcon } from "lucide-react"
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
+
+function Checkbox({
+  className,
+  ...props
+}: React.ComponentProps<typeof CheckboxPrimitive.Root>) {
+  return (
+    <CheckboxPrimitive.Root
+      data-slot="checkbox"
+      className={cn(
+        // "-mt-1",
+        'flex rounded-sm shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground',
+        "peer border-input",
+        "ring-ring/10 dark:ring-ring/20 dark:outline-ring/40 outline-ring/50",
+        "rounded-[4px] focus-visible:ring-2 focus-visible:outline-1",
+        "data-[state=checked]:border-primary size-4 shrink-0 border transition-[color,box-shadow] aria-invalid:focus-visible:ring-0",
+        className
+      )}
+      {...props}
+    >
+      <CheckboxPrimitive.Indicator
+        data-slot="checkbox-indicator"
+        className="flex items-center justify-center text-current"
+      >
+        <CheckIcon className="size-3.5" />
+      </CheckboxPrimitive.Indicator>
+    </CheckboxPrimitive.Root>
+  )
+}
+
+export { Checkbox }
--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,19 +1,26 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
 
-  const env = loadEnv(mode, mode === 'development'
-    ? process.cwd().replace('/apps/frontend', '')
-    : process.cwd())
+  const loadEnvDir = mode === 'development' ? process.cwd().replace('/apps/frontend', '') : process.cwd()
+
+  const env = loadEnv(mode, loadEnvDir)
 
   return {
     plugins: [
       react(),
       tailwindcss(),
+      TanStackRouterVite({
+        autoCodeSplitting: true,
+        routesDirectory: './src/router/routes',
+        generatedRouteTree: './src/routeTree.gen.ts',
+      }),
     ],
+    envDir: loadEnvDir,
 
     server: {
       open: true,
